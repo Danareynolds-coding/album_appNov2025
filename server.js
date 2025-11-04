ebaab9d65cd40608ac2af125e47d54b23cc3000f
Danareynolds-coding <dana.reynolds@mscoding.org>
2025-11-03 19:19:34 -0600

    added helmet and cors; dbconfig

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
-//step1
+//step1 build server
 const express = require('express')
 const server = express()
 const PORT = process.env.PORT || 3000
-
-
+//step2 handle security
+const helmet = require('helmet')
+const cors = ('cors')
+// step2 dont use >server.use(helmet())configuring helmet below
+server.use(helmet.contentSecurityPolicy({
+    useDefaults:true,
+    crossOriginResourcePolicy:false,
+    crossOriginEmbedderPolicy:false,
+    directives:{
+      "img-src":["'self'", "https: data"],
+      "scriptSrc":["'self'","cdn.jsdelivr.net"]
+    }
+}))
+server.use(cors())
+//step3 json and url setup
+server.use(express.json())
+server.use(express.urlencoded({extended:true}))
 //step1
 server.listen(PORT, ()=> console.log(`Server is at port ${PORT}!!!!`))
