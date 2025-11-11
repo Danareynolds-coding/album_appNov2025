e66a60f329d0c0b8bbcb01208183bb9e6b446623
Danareynolds-coding <dana.reynolds@mscoding.org>
2025-11-11 11:44:56 -0600

    update adding create method

--- daos/Common/daoCommon.js
+++ daos/Common/daoCommon.js
@@ -29,9 +29,39 @@ const daoCommon = {
           queryAction(res, error, rows, table)
         }
       ) 
-    }   
+    },
+  create: (req, res, table)=> {
+   if (Object.keys(req.body).length === 0){
+    res.json({
+      "error": true, 
+      "message":"no fiels to create"
+     })
+   }else{
+    const fields = Object.keys(req.body)
+    //returns an array
+    const values = Object.values(req.body)
+    //returns an array  & execute takes 3 args 
+    connect.execute(
+      `INSERT INTO ${table} SET ${fields.join(' = ?, ')} = ?;`,    
+      //.join each fields eg title set to ? to equal ?(? pulls from value array )example
+      //fields = [name, age, occupation]
+      //value = [dana, 61, developer]
+      values,
+      (error, dbres)=>{
+        if(!error){
+          res.json({
+            //dbres(databaseres.insertedId)
+            Last_id: dbres.insertedId
+          })
+
+        }else{
+          console.log('${table')
+        }
+      }
+    )
+   }
+  }
 }
-  
 
 
   module.exports = daoCommon
